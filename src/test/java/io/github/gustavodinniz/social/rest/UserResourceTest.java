90c64589586832b366aa7eb371d891c51793d6a7
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-05-19 14:15:02 -0300

    testing validation error case

--- src/test/java/io/github/gustavodinniz/social/rest/UserResourceTest.java
+++ src/test/java/io/github/gustavodinniz/social/rest/UserResourceTest.java
@@ -1,10 +1,14 @@
 package io.github.gustavodinniz.social.rest;
 
 import io.github.gustavodinniz.social.rest.dto.CreateUserRequest;
+import io.github.gustavodinniz.social.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,5 +33,28 @@ class UserResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("should return error when json is not valid")
+    public void createUserValidationErrorTest() {
+        var user = new CreateUserRequest();
+        user.setName(null);
+        user.setAge(null);
+
+        var response = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
     }
 }
